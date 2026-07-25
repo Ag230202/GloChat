0a98a7caed8bdb1247ba1b83b36df382a5638f63
ANIMESH GARTIA <109869961+Ag230202@users.noreply.github.com>
2026-07-26 00:21:20 +0530

    Redis implementation

--- server/settings.py
+++ server/settings.py
@@ -127,9 +127,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ASGI_APPLICATION = 'server.asgi.application'
 
+import os
+
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379/0')],
+        },
     },
 }
 
